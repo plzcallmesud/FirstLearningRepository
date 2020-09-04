6cbcaccd1c799e56016cc48a3df689d7c73bcedd
plzcallmesud <48294255+plzcallmesud@users.noreply.github.com>
2020-09-04 10:49:10 -0400

    add one property in js file
    test data

--- SalesforceTechnoWarrior/force-app/main/default/lwc/helloSalesforce/helloSalesforce.js
+++ SalesforceTechnoWarrior/force-app/main/default/lwc/helloSalesforce/helloSalesforce.js
@@ -3,5 +3,6 @@ import { LightningElement, api, track, wire } from 'lwc';
 export default class HelloSalesforce extends LightningElement {
     @api prop1 = 'Salesforce Techno Warrior'; // Defining default value for configurable property 
    @api prop2 = false;
+   @api prop4 = false;
    //To expose a public property, decorate it with @api. 
 }

b2c68fadf23a44a6cbad58f266cff82c36aef829
plzcallmesud <48294255+plzcallmesud@users.noreply.github.com>
2020-09-09 09:38:28 -0400

    added code for trailhead

--- SalesforceTechnoWarrior/force-app/main/default/lwc/helloSalesforce/helloSalesforce.js
+++ SalesforceTechnoWarrior/force-app/main/default/lwc/helloSalesforce/helloSalesforce.js
@@ -1,8 +1,7 @@
-import { LightningElement, api, track, wire } from 'lwc';
-
-export default class HelloSalesforce extends LightningElement {
-    @api prop1 = 'Salesforce Techno Warrior'; // Defining default value for configurable property 
-   @api prop2 = false;
-   @api prop4 = false;
-   //To expose a public property, decorate it with @api. 
+import { LightningElement } from 'lwc';
+export default class HelloWorld extends LightningElement {
+  greeting = 'World';
+  changeHandler(event) {
+    this.greeting = event.target.value;
+  }
 }
